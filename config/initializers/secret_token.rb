3bbb12fd6872c390d82466b59c8173950cc1fa57
jasonklein <jasonklein@gmail.com>
2014-08-29 13:10:31 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BgDryRunApp::Application.config.secret_token = '023bcf0cf03b03cb75d891c6fce73b72647f7b2026ef77f56759d72d3f5b9cb3e3f85a7084625fb4cd78fd3e156c74a3ffc96dfbf6060445df9e88f0a08290bd'
