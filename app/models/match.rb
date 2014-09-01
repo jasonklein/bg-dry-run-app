c5a3c9bacf8669f9ada0cc5b6e8551f88a3d60da
jasonklein <jasonklein@gmail.com>
2014-09-01 20:05:42 +0100

    Add associations to models; adjust form to reflect changes to models and matches controller.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,10 @@
 class Match < ActiveRecord::Base
-  attr_accessible :video, :name
+  attr_accessible :name, :play_date, :videos_attributes
 
 
   has_many :clips
-  has_one :video
-  
+  has_many :videos
+
+  accepts_nested_attributes_for :videos
+
 end
