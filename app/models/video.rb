280a95e8418b9185b6f79de689f3b5090c4ac6f2
jasonklein <jasonklein@gmail.com>
2014-09-05 19:41:01 +0100

    Add view helper for displaying video duration.

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,8 @@ class Video < ActiveRecord::Base
   attr_accessible :audio_channels, :audio_sample_rate, :audio_stream, :bitrate, :container, :creation_time, :duration, :height, :location, :video_codec, :video_stream, :width, :camera
 
   mount_uploader :location, MatchVideoUploader
+
+  def duration_in_minutes
+    (self.duration / 60).round(2)
+  end
 end
