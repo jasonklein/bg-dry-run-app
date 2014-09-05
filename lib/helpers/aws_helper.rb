e3db5fc85163eaff853610707a5393d4b0b9d5ae
jasonklein <jasonklein@gmail.com>
2014-09-05 23:44:11 +0100

    Further develop tags rake task and add AWSHelper module.

---
+++ lib/helpers/aws_helper.rb
@@ -0,0 +1,71 @@
+module AWSHelper
+  def aws_pipeline_id
+    ENV["EC2_TRANSCODE_PIPELINE_ID_#{Rails.env.upcase}"]
+  end
+
+  def aws_region
+    "eu-west-1"
+  end
+
+  def aws_web_preset_id
+    "1351620000001-100070"
+  end
+
+  def aws_transcoder_client
+    if !@transcoder_client
+      @transcoder_client = AWS::ElasticTranscoder::Client.new(region: aws_region)
+    end
+    @transcoder_client
+  end
+
+  def aws_input(path)
+    input = { key: path }
+  end
+
+  def aws_output_key(upi)
+    output_key_suffix = Digest::SHA256.hexdigest(input_key.encode('UTF-8'))[0..10]
+    output_key = "bg-clip-#{upi.downcase}-#{output_key_suffix}"
+    output_key
+  end
+
+  def aws_output_key_prefix(match_id, video_id)
+    "ectranscoder/output/match/#{match_id}/video/#{video_id}/"
+  end
+
+  def aws_output_composition(video, tag_time)
+    start_time = tag_time - 5
+    if start_time < video.creation_time
+      start_time = video.creation_time
+    end
+
+    duration = 20
+    video_end_time = video.creation_time + video.duration
+    if (start_time + duration) > video_end_time
+      start_time = video_end_time - duration
+    end
+    {timespan: {start_time: start_time, duration: duration}}
+  end
+
+  def make_transcoded_clip(video, tag_time, upi)
+    clip_output = {
+      key: aws_output_key(upi),
+      preset_id: aws_web_preset_id,
+      composition: aws_output_composition(video, tag_time)
+    }
+    outputs = [clip_output]
+
+    # aws_transcoder_client.create_job(
+    #   pipeline_id: aws_pipeline_id,
+    #   input: aws_input(video.location.path),
+    #   output_key_prefix: aws_output_key_prefix(video.match_id, video.id),
+    #   outputs: outputs
+    #   )
+
+    {
+      pipeline_id: aws_pipeline_id,
+      input: aws_input(video.location.path),
+      output_key_prefix: aws_output_key_prefix(video.match_id, video.id),
+      outputs: outputs
+      }
+  end
+end
--- lib/tasks/tags.rake
+++ lib/tasks/tags.rake
@@ -1,20 +1,27 @@
+require_relative '../helpers/aws_helper'
+include AWSHelper
+
 desc "Make clips and transcode via AWS"
 task :tags => :environment do
   BaseModule.unrun.each do |bm|
     split_tags = bm.split_tags
     split_tags.each do |split_tag|
       upi = split_tag[0..1].join
-      match_id = bm.match_id
-      if player = Player.where(upi: upi, match_id: match_id).first
-        clip = Clip.new
-        clip.match_id = match_id
-        clip.player_id = player.id
-        clip.base_module_id = bm.id
-        clip.tag_time = split_tag[2..3].join " "
-        clip.save
-      else
-        return
-      end
+      if match = bm.match
+        if player = Player.where(upi: upi, match_id: match.id).first
+          match.videos.each do |video|
+            clip = Clip.new
+            clip.video_id = video.id
+            clip.match_id = match.id
+            clip.player_id = player.id
+            clip.base_module_id = bm.id
+            clip.tag_time = split_tag[2..3].join " "
+            clip.save
+          end
+        else
+          return
+        end
+      end 
     end
     bm.update_attributes(report_run: true)
   end
